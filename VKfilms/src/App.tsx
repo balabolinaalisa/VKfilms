4217f5d94ef9bddc842c68d4674dd7fbf48717c0
balabolinaalisa <90497059+alisaaaaaaaaa@users.noreply.github.com>
2025-07-14 23:04:33 +0300

    исправлен роутинг

--- VKfilms/src/App.tsx
+++ VKfilms/src/App.tsx
@@ -1,25 +1,19 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
+
 import Home from './pages/Home';
 import Favorites from './pages/Favorites';
 import MovieDetails from './pages/MovieDetails';
-import { AppRoot, ConfigProvider, AdaptivityProvider } from '@vkontakte/vkui';
 
-function App() {
-  return (
-    <ConfigProvider>
-      <AdaptivityProvider>
-        <AppRoot>
-          <BrowserRouter>
+function App(){
+    return(
+        <div className="min-h-screen w-full bg-gray-100 p-0 m-0">
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/favorites" element={<Favorites />} />
               <Route path="/movie/:id" element={<MovieDetails />} />
             </Routes>
-          </BrowserRouter>
-        </AppRoot>
-      </AdaptivityProvider>
-    </ConfigProvider>
-  );
+        </div>
+    );
 }
 
 export default App;
